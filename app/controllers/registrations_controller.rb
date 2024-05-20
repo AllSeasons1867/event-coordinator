b6329777db44b6499e1ba917fb3ddde2bd364785
FW <35238548+AllSeasons1867@users.noreply.github.com>
2024-05-19 18:36:02 -0600

    Refactored event find logic in registrations controller'

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,20 @@
 class RegistrationsController < ApplicationController
+    before_action :set_event 
+
     def index
-        @event = Event.find(params[:event_id])
+
         @registrations = @event.registrations
     end
 
     def new 
-        @event = Event.find(params[:event_id])
+
         @registration = @event.registrations.new
     end
 
     def create 
-        @event = Event.find(parms[:event_id])
 
-        @registration = @event.registration.new(registration_params)
+
+        @registration = @event.registrations.new(registration_params)
 
         if @registration.save
             redirect_to event_registrations_url(@event),
@@ -26,5 +28,7 @@ class RegistrationsController < ApplicationController
     def registration_params
         params.require(:registration).permit(:name, :email, :how_heard)
     end
-
+    def set_event 
+        @event = Event.find(params[:event_id])
+    end
 end
